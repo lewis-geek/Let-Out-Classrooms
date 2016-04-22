9ed5e92f574601eb4957a28976fb4b38475ec46d
lewis-geek <lewis_geek@icloud.com>
2016-04-23 00:24:46 +0800

    添加了顶部搜索栏下拉菜单

---
+++ script/search.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Lewis on 16/4/22.
+ */
+$(document).ready(function () {
+    /*
+     animation
+     */
+    (function () {
+        $('#search input').focus(function () {
+            $('#search').css('background-color', '#f9f9f9');
+        });
+        $('#search input').focusout(function () {
+            $('#search').css('background-color', 'transparent');
+        })
+    })();
+
+});
